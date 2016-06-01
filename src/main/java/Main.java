f044904291cf365629c13c2d3d41fc3a2873e9a3
Nick <makarovmykola@gmail.com>
2016-06-01 22:36:53 +0300

    wrong string saving added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,17 @@ public class Main extends Calculator {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, input math expression: ");
+
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Please, input math expression: ");
             String inputLine = scanner.nextLine();
             if (inputLine.equals("exit")) {
                 break;
+            } else if (!inputLine.contains("+") && !inputLine.contains("-")
+                    && !inputLine.contains("*") && !inputLine.contains("/")) {
+                System.out.println("Please enter math operation with +, -, * or /");
+                inputLine = scanner.nextLine();
             }
             try {
                 double result = execute(inputLine);
